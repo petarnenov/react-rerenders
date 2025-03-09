15915dff6522d85f4b260948961e000db879fdaf
Petar Nenov <petarnenovpetrov@gmail.com>
2025-03-09 15:53:02 +0200

    add links

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+//https://www.youtube.com/watch?v=4MmmlWwlST4&ab_channel=JackHerrington
+//https://www.youtube.com/watch?v=INLq9RPAYUw&t=554s&ab_channel=Theo-t3%E2%80%A4gg
+//https://react-scan.com/
 import React from 'react'
 import Counter from './components/Counter'
 import ColorPicker from './components/ColorPicker'
